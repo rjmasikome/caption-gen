88835622f6541f4bc3f7ed8d3efa90ebec57b02d
rjmasikome <ryan.josh.m@gmail.com>
2016-06-02 22:53:15 +0200

    Updated

--- caption-gen.py
+++ caption-gen.py
@@ -4,11 +4,18 @@ import urllib2
 import json
 import re
 import random
+import mraa
 
-print "Getting some quotes online..."
+# digital input - button in D6
+touchPin = mraa.Gpio(6)
+touchPin.dir(mraa.DIR_IN)
+
+# digital output - buzzer in D8
+buzPin = mraa.Gpio(8) 
+buzPin.dir(mraa.DIR_OUT)
 
 # Some invalid json repair: http://stackoverflow.com/questions/15198426/fixing-invalid-json-escape
-invalid_escape = re.compile(r'\\[0-7]{1,3}')  # up to 3 digits for byte values up to FF
+invalid_escape = re.compile(r'\\[0-7]{1,6}')  # up to 3 digits for byte values up to FF
 
 def replace_with_byte(match):
         return chr(int(match.group(0)[1:], 8))
@@ -17,46 +24,60 @@ def repair(brokenjson):
             return invalid_escape.sub(replace_with_byte, brokenjson) 
 #################################################################
 
-while True:
-    url = 'http://api.forismatic.com/api/1.0/'
-    params = 'method=getQuote&key=457653&format=json&lang=en'
+while 1:
+    while touchPin.read() == 1:
+        buzPin.write(1)
+        sleep(1)
+        buzPin.write(0)
 
-    respJson = urllib2.urlopen(url, params).read()
-    #Repair invalid json escape character
-    repairMap = repair(respJson)
-    respMap = json.loads(repairMap)
+        sleep(1)
 
-    print "Quotes received!"
+        print "Getting some quotes online..."
 
-    quoteText = respMap["quoteText"]
-    quoteAuthor = respMap["quoteAuthor"]
-    quote = list(quoteText + ' -' + quoteAuthor)
+        url = 'http://api.forismatic.com/api/1.0/'
+        params = 'method=getQuote&key=457653&format=json&lang=en'
 
-    myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
-    myLcd.setColor(random.randrange(0,255), random.randrange(0,255), random.randrange(0,255))
+        respJson = urllib2.urlopen(url, params).read()
+        #Repair invalid json escape character
+        repairMap = repair(respJson)
+        respMap = json.loads(repairMap)
 
-    line = 0
+        print "Quotes received!"
 
-    for i in range(len(quote)):
-        if i%16 == 0:
-            if i%32 == 0:
-                sleep(0.7)
-                myLcd.setCursor(0,0)
-                myLcd.write("                ")
-                myLcd.setCursor(1,0)
-                myLcd.write("                ")
-            if i > 0:
-                line += 1
-        line = line % 2
-        myLcd.setCursor(line,i%16)
-        myLcd.write(str(quote[i]))
-        sleep(0.2)
+        quoteText = respMap["quoteText"]
+        quoteAuthor = respMap["quoteAuthor"]
+        quote = list(quoteText + ' -' + quoteAuthor)
+        # quote = quoteText + ' -' + quoteAuthor
 
-    sleep(2)
-    myLcd.setCursor(0,0)
-    myLcd.write("                ")
-    myLcd.setCursor(1,0)
-    myLcd.write("                ")
-    sleep(60)
+        myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+        myLcd.setColor(random.randrange(0,255), random.randrange(0,255), random.randrange(0,255))
+        myLcd.cursorBlinkOn()
 
+        # myLcd.autoScrollOn()
+        # myLcd.setCursor(0,0)
+        # myLcd.write(quote)
+
+        line = 0
+
+        for i in range(len(quote)):
+            if i%16 == 0:
+                if i%32 == 0:
+                    # clear the whole screen if the LCD is fully occupied
+                    sleep(0.7)
+                    myLcd.setCursor(0,0)
+                    myLcd.write("                ")
+                    myLcd.setCursor(1,0)
+                    myLcd.write("                ")
+                if i > 0:
+                    line += 1
+            line = line % 2
+            myLcd.setCursor(line,i%16)
+            myLcd.write(str(quote[i]))
+            sleep(0.2)
+
+        sleep(3)
+
+        myLcd.clear()
+        myLcd.cursorBlinkOff()
+        myLcd.setColor(0,0,0)
 
